9695608fd437307bb3484e3b18c2f1dd4059151c
Kittipat Jantasila <kittipat050871@gmail.com>
2026-08-10 00:11:20 +0700

    Clean repo: untrack build artifacts, db, logs + update gitignore

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- /home/kittipat/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/kittipat/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
